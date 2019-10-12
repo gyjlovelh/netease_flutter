a5156eadc31027f67b28fe83de6723cc1febf1fc
TS_02 <gong_xyu@163.com>
2019-10-12 16:06:50 +0800

    “我的”模块：icons栏目

---
+++ lib/pages/home/user_center/icon_buttons_item.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:netease_flutter/shared/pages/icon_data/icon_data.dart';
+import './icon_buttons_vo.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class IconButtonsItem extends StatelessWidget {
+  IconButtonsVO itemVO;
+
+  IconButtonsItem({@required this.itemVO});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 6.0,right: 6.0),
+      child: GestureDetector(
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(
+                  bottom: ScreenUtil.getInstance().setHeight(15.0)),
+              padding: EdgeInsets.all(ScreenUtil.getInstance().setWidth(24.0)),
+              decoration: BoxDecoration(
+                  color: Colors.redAccent,
+                  borderRadius: BorderRadius.circular(99.0)),
+              child: NeteaseIconData(
+                itemVO.pointer,
+                color: Colors.white,
+                size: ScreenUtil.getInstance().setSp(42.0),
+              ),
+            ),
+            Text(itemVO.label,
+                style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: ScreenUtil.getInstance().setSp(24.0)))
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/home/user_center/icon_buttons_vo.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class IconButtonsVO {
+    int pointer;
+    String label;
+    IconButtonsVO({@required this.pointer,@required this.label});
+}
