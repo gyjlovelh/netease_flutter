a1a37b5f65ae9c489c64f0369f1fea472b5fb7fa
Xiaoyu Gong <gong_xyu@163.com>
2019-10-15 21:40:37 +0800

    mine

--- lib/pages/home/user_center/icon_buttons_mine.dart
+++ lib/pages/home/user_center/icon_buttons_mine.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import './icon_buttons_item.dart';
 import './icon_buttons_vo.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 Widget iconButtonsMine(List<IconButtonsVO> list) {
   return Container(
+    // height: ScreenUtil().setHeight(40.0),
     child: ListView.builder(
       scrollDirection: Axis.horizontal,
       itemCount: list.length,
--- lib/pages/home/user_center/user_center.dart
+++ lib/pages/home/user_center/user_center.dart
@@ -3,6 +3,7 @@ import 'package:netease_flutter/pages/home/user_center/icon_buttons_item.dart';
 import './icon_buttons_mine.dart';
 import './icon_buttons_vo.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../shared/pages/icon_data/icon_data.dart';
 
 class NeteaseUserCenter extends StatefulWidget {
   @override
@@ -10,22 +11,21 @@ class NeteaseUserCenter extends StatefulWidget {
 }
 
 class _NeteaseUserCenterState extends State<NeteaseUserCenter> {
-
   //ListView Item
   List<IconButtonsVO> getListData() {
     List<IconButtonsVO> list = List<IconButtonsVO>();
-    list.add(IconButtonsVO(pointer:0xe637,label:'云村正能量'));
-    list.add(IconButtonsVO(pointer:0xe610,label:'私人FM'));
-    list.add(IconButtonsVO(pointer:0xeabd,label:'最嗨电音'));
-    list.add(IconButtonsVO(pointer:0xe638,label:'Sati空间'));
-    list.add(IconButtonsVO(pointer:0xe609,label:'私藏推荐'));
-    list.add(IconButtonsVO(pointer:0xe64a,label:'因乐交友'));
-    list.add(IconButtonsVO(pointer:0xe68c,label:'亲子频道'));
-    list.add(IconButtonsVO(pointer:0xe723,label:'古典专区'));
-    list.add(IconButtonsVO(pointer:0xe611,label:'跑步FM'));
-    list.add(IconButtonsVO(pointer:0xe762,label:'小冰电台'));
-    list.add(IconButtonsVO(pointer:0xe60e,label:'爵士电台'));
-    list.add(IconButtonsVO(pointer:0xe7c8,label:'驾驶模式'));
+    list.add(IconButtonsVO(pointer: 0xe637, label: '云村正能量'));
+    list.add(IconButtonsVO(pointer: 0xe610, label: '私人FM'));
+    list.add(IconButtonsVO(pointer: 0xeabd, label: '最嗨电音'));
+    list.add(IconButtonsVO(pointer: 0xe638, label: 'Sati空间'));
+    list.add(IconButtonsVO(pointer: 0xe609, label: '私藏推荐'));
+    list.add(IconButtonsVO(pointer: 0xe64a, label: '因乐交友'));
+    list.add(IconButtonsVO(pointer: 0xe68c, label: '亲子频道'));
+    list.add(IconButtonsVO(pointer: 0xe723, label: '古典专区'));
+    list.add(IconButtonsVO(pointer: 0xe611, label: '跑步FM'));
+    list.add(IconButtonsVO(pointer: 0xe762, label: '小冰电台'));
+    list.add(IconButtonsVO(pointer: 0xe60e, label: '爵士电台'));
+    list.add(IconButtonsVO(pointer: 0xe7c8, label: '驾驶模式'));
     return list;
   }
 
@@ -36,7 +36,38 @@ class _NeteaseUserCenterState extends State<NeteaseUserCenter> {
       child: Column(
         children: <Widget>[
           Expanded(
-            child:iconButtonsMine(getListData()) ,
+            child: iconButtonsMine(getListData()),
+          ),
+          mDivider(0.5),
+          userActionItem(0xe680, '本地音乐'),
+          mDivider(0.5),
+          userActionItem(0xe625, '最近播放'),
+          mDivider(0.5),
+          userActionItem(0xe691, '下载管理'),
+          mDivider(0.5),
+          userActionItem(0xe620, '我的电台'),
+          mDivider(0.5),
+          userActionItem(0xe621, '我的收藏'),
+          mDivider(20.0),
+          Row(
+            children: <Widget>[
+              Expanded(
+                flex: 0,
+                child: IconButton(icon: NeteaseIconData(0xe626),onPressed: (){},),
+              ),
+              Expanded(
+                flex: 1,
+                child: Text('创建的歌单',style: TextStyle(fontSize: ScreenUtil().setSp(30.0),fontWeight: FontWeight.w700),),
+              ),
+              Expanded(
+                flex: 0,
+                child: IconButton(icon: NeteaseIconData(0xe64b), onPressed: () {},),
+              ),
+              Expanded(
+                flex: 0,
+                child: IconButton(icon: NeteaseIconData(0xe62b),onPressed: (){},),
+              ),
+            ],
           ),
         ],
       ),
@@ -44,3 +75,34 @@ class _NeteaseUserCenterState extends State<NeteaseUserCenter> {
   }
 }
 
+Widget userActionItem(int pointer, String title) {
+  return Container(
+    width: double.infinity,
+    child: GestureDetector(
+      onTap: () {},
+      child: Row(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(
+                left: 25.0, right: 25.0, top: 12.0, bottom: 12.0),
+            child: NeteaseIconData(
+              pointer,
+              color: Colors.black,
+            ),
+          ),
+          Text(
+            title,
+            style: TextStyle(fontSize: ScreenUtil().setSp(25.0)),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+Widget mDivider(double h) {
+  return Divider(
+    height: ScreenUtil().setHeight(h),
+    color: Colors.grey,
+  );
+}
